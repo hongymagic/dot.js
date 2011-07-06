a717803d421aa2c0f00751e6e34ff9b02024e413
David G. Hong <d.hong@me.com>
2011-07-06 22:28:42 +1000

    more work on jibbering.com

--- jibbering.com.js
+++ jibbering.com.js
@@ -16,16 +16,38 @@
 		});
 	}
 
-  /* change typography */
-  {
-    log('Changing typography');
-    $('html,body,h1,h2,h3,h4,h5,p').css({
-      'font': 'normal normal 400 1em/1.5em Georgia, Serif'
-    });
-    $('h1,h2').css({
-      'font-size': '135%',
-      'line-height': '140%'
-    });
-  }
+	/* change typography */
+	log('Changing typography');
+	var baseSize = 100,
+	    baseUnit = '%',
+	    getScaledValue = function(increase) {
+		    return (baseSize * increase) + baseUnit;
+	    };
+
+	$('html').css({
+		'font': 'normal normal 400 1em/1.5em Georgia, Serif'
+	});
+	$('h1,h2').css({
+		'font-size': getScaledValue(1.4),
+		'line-height': getScaledValue(1.45)
+	});
+	$('h3,h4').css({
+		'font-size': getScaledValue(1.2),
+		'line-height': getScaledValue(1.23)
+	});
+
+	/* staticly positioned ToC */
+	log('Enhancing table of contents');
+	var css = '#faqNav + ul { display: none; margin: 0; border-right: solid 1px #333; border-bottom: solid 1px #333; padding: 0; position: fixed; top: 0; left: 0; }';
+	var style = document.createElement('style'),
+	    rules = document.createTextNode(css);
+
+	style.type = 'text/css';
+	if (style.styleSheet) {
+		style.styleSheet.cssText = rules.nodeValue;
+	} else {
+		style.appendChild(rules);
+	}
+	document.getElementsByTagName('head')[0].appendChild(style);
 })();
 
